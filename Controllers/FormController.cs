f356b70f99683db6368eb7e7075715a5f6dccb7a
Darexolu <darexolu16@gmail.com>
2023-08-08 17:56:45 +0100

    update and delete added

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -23,11 +23,88 @@ namespace EmailWebApplication.Controllers
         [HttpPost]
         public IActionResult Create(EmailForm obj)
         {
-            _db.EmailForms.Add(obj);
+            if(obj.FirstName == obj.Email.ToString())
+            {
+                ModelState.AddModelError("FirstName", "The First Name cannot exactly match the Email");
+            }
+            //if (obj.FirstName != null && obj.FirstName.ToLower() == "test"){
+            //    ModelState.AddModelError("", "Test is an invalid value");
+
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _db.EmailForms.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+              
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id== null || id== 0)
+            {
+                return NotFound();
+            }
+            EmailForm emailFormDB = _db.EmailForms.Find(id);
+            //EmailForm emailFormDB1 = _db.EmailForms.FirstOrDefault(u=>u.ID==id);
+
+            //EmailForm emailFormDB2 = _db.EmailForms.Where(u => u.ID == id).FirstOrDefault();
+
+            if (emailFormDB == null)
+            {
+                return NotFound();
+            }
+            return View(emailFormDB);
+        }
+        [HttpPost]
+        public IActionResult Edit(EmailForm obj)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                _db.EmailForms.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            EmailForm? emailFormDB = _db.EmailForms.Find(id);
+
+            if (emailFormDB == null)
+            {
+                return NotFound();
+            }
+            return View(emailFormDB);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            EmailForm? emailFormDB2 = _db.EmailForms.Find(id);
+            if (emailFormDB2 == null)
+            {
+                return NotFound();
+            }
+            _db.EmailForms.Remove(emailFormDB2);
             _db.SaveChanges();
             return RedirectToAction("Index");
+
            
         }
+
 
     }
 }
--- Models/EmailForm.cs
+++ Models/EmailForm.cs
@@ -1,11 +1,17 @@
-﻿namespace EmailWebApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace EmailWebApplication.Models
 {
     public class EmailForm
     {
         public int ID { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
+        [MaxLength(50)]
         public string LastName { get; set; }
         public string Gender { get; set; }
+        //[EmailAddress]
+        //[RegularExpression(@"^[A-Za-z0-9]+@([a-zA-Z]+\\.)+[a-zA-Z]{2,6}]&")]
         public string Email { get; set; }
 
     }
